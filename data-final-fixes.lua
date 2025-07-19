ad1c9b53f8a8e9877af4b04510bdb7d96d9cd39f
Bong_Cat <vanomamsik@gmail.com>
2025-07-19 23:29:32 +0300

    Блядота всё равно не работает

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,17 +1,13 @@
 require("prototypes.recipes")
 require("prototypes.technology")
--- Базовые ванильные поезда
 data.raw["locomotive"]["locomotive"].order = "c[rolling-stock]-a[locomotive]"
 data.raw["cargo-wagon"]["cargo-wagon"].order = "c[rolling-stock]-b[cargo-wagon]"
 data.raw["fluid-wagon"]["fluid-wagon"].order = "c[rolling-stock]-c[fluid-wagon]"
 
--- Твои мини-поезда
 data.raw["locomotive"]["mini-locomotive"].order = "c[rolling-stock]-d[mini-locomotive]"
 data.raw["cargo-wagon"]["mini-cargo-wagon"].order = "c[rolling-stock]-e[mini-cargo-wagon]"
 data.raw["fluid-wagon"]["mini-fluid-wagon"].order = "c[rolling-stock]-f[mini-fluid-wagon]"
 
--- Пример исправления подгруппы для мини-поезда
 data.raw["locomotive"]["mini-locomotive"].subgroup = "train-transport"
 data.raw["cargo-wagon"]["mini-cargo-wagon"].subgroup = "train-transport"
 data.raw["fluid-wagon"]["mini-fluid-wagon"].subgroup = "train-transport"
-
